0bf3eedc7d608d8c2449d0ec4960408aead6ecbf
Yasen Atanasov <y.atanasoff@gmail.com>
2023-10-03 16:30:57 +0300

    astgen initial

---
+++ astgen/ASTGen.cs
@@ -0,0 +1,15 @@
+﻿namespace ASTGen
+{
+    public static class ASTGen
+    {
+        public static void Main(string[] args)
+        {
+            if(args.Length != 1)
+            {
+                Console.WriteLine("Usage: astgen <output directory>");
+                System.Environment.Exit(64);
+            }
+        }
+    }
+}
+
