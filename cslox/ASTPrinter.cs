5d57386c2ee2a39a3df132e83b3611442b9e9c58
Yasen Atanasov <y.atanasoff@gmail.com>
2023-10-05 17:12:20 +0300

    added AST printer

---
+++ cslox/ASTPrinter.cs
@@ -0,0 +1,45 @@
+using System.Text;
+
+namespace Lox
+{
+    public class ASTPrinter : Expr.Visitor<string>
+    {
+        public string Print(Expr expr)
+        {
+            return expr.Accept(this);
+        }
+
+        public string VisitBinary(Expr.Binary expr)
+        {
+            return Parenthesize(expr.Operator.Lexeme, expr.Left, expr.Right);
+        }
+
+        public string VisitGrouping(Expr.Grouping expr)
+        {
+            return Parenthesize("group", expr.Expression);
+        }
+
+        public string VisitLiteral(Expr.Literal expr)
+        {
+            return (expr?.Value.ToString()) ?? "nil";
+        }
+
+        public string VisitUnary(Expr.Unary expr)
+        {
+            return Parenthesize(expr.Operator.Lexeme, expr.Right);
+        }
+
+        private string Parenthesize(string name, params Expr[] exprs)
+        {
+            StringBuilder builder = new();
+            builder.Append('(').Append(name);
+            foreach (var expr in exprs)
+            {
+                builder.Append(' ').Append(expr.Accept(this));
+            }
+            builder.Append(')');
+
+            return builder.ToString();
+        }
+    }
+}
