2b060d0a67d73cba03508cc2067c92f53c01151a
Yasen Atanasov <y.atanasoff@gmail.com>
2023-10-05 18:06:30 +0300

    added RPN printer

---
+++ cslox/RPNPrinter.cs
@@ -0,0 +1,44 @@
+using System.Text;
+
+namespace Lox
+{
+    public class RPNPrinter : Expr.Visitor<string>
+    {
+        public string Print(Expr expr)
+        {
+            return expr.Accept(this);
+        }
+
+        public string VisitBinary(Expr.Binary expr)
+        {
+            return RPN(expr.Operator.Lexeme, expr.Left, expr.Right);
+        }
+
+        public string VisitGrouping(Expr.Grouping expr)
+        {
+            return RPN("g", expr.Expression);
+        }
+
+        public string VisitLiteral(Expr.Literal expr)
+        {
+            return (expr?.Value.ToString()) ?? "nil";
+        }
+
+        public string VisitUnary(Expr.Unary expr)
+        {
+            return RPN(expr.Operator.Lexeme, expr.Right);
+        }
+
+        private string RPN(string name, params Expr[] exprs)
+        {
+            StringBuilder builder = new();
+            foreach (var expr in exprs)
+            {
+                builder.Append(expr.Accept(this)).Append(' ');
+            }
+
+            builder.Append(name);
+            return builder.ToString();
+        }
+    }
+}
