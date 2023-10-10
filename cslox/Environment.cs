abd76931a89cb561665784b4a7ba987ce1264e5c
Yasen Atanasov <y.atanasoff@gmail.com>
2023-10-10 15:22:19 +0300

    environment

---
+++ cslox/Environment.cs
@@ -0,0 +1,22 @@
+namespace Lox
+{
+    public class Environment
+    {
+        private readonly Dictionary<string, object> values = new();
+
+        public void Define(string name, object value)
+        {
+            values[name] = value;
+        }
+
+        public object Get(Token name)
+        {
+            if(values.TryGetValue(name.Lexeme, out object? value))
+            {
+                return value;
+            }
+            
+            throw new RuntimeException(name, $"Undefined variable '{name.Lexeme}'.");
+        }
+    }
+}
